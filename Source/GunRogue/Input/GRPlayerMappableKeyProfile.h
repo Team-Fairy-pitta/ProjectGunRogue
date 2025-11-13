3f00a09167b241586f1a75672d7ea914f0b51664
Daegeun Choe <asdf12gg@gmail.com>
2025-11-13 20:05:23 +0900

    사용자가 원하는 키를 설정할 수 있는 Key Setting Widget을 개발 (#36)
    * 입력 관련 uasset 생성
    
    * IMC, Input Tag 지정 완료
    
    * Input Setting UI 개발중
    
    * Input User Setting 관련 클래스 상속
    
    * User Setting, Key Mappable 정보 추가
    
    * InputComponent를 이용해서 초기 입력 설정을 할 때, UserSetting을 불러오는 과정, 그리고 저장하는 과정을 추가로 진행한다
    
    * 키 세팅을 위한 위젯 추가 구현
    
    * 키 설정, 카테고리 추가
    
    * 키 매핑 적용 완료 (실시간으로 적용 안 되는 것 까지, 테스트 할 때와 동일하게 적용 완료)
    
    * 실시간 적용이 되지 않던 문제, 간단하게 해결
    
    * default, clear, all reset 버튼 기능 구현
    
    * 오타 수정 (parrent -> parent)

---
+++ Source/GunRogue/Input/GRPlayerMappableKeyProfile.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "UserSettings/EnhancedInputUserSettings.h"
+#include "GRPlayerMappableKeyProfile.generated.h"
+
+UCLASS()
+class GUNROGUE_API UGRPlayerMappableKeyProfile : public UEnhancedPlayerMappableKeyProfile
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/GunRogue/UI/KeySetting/GRKeySettingSlot.cpp
@@ -0,0 +1,146 @@
+#include "UI/KeySetting/GRKeySettingSlot.h"
+#include "UI/KeySetting/GRKeySettingWidget.h"
+#include "Components/TextBlock.h"
+#include "Components/Button.h"
+#include "Components/InputKeySelector.h"
+
+void UGRKeySettingSlot::NativeConstruct()
+{
+	if (!InputKeySelector)
+	{
+		UE_LOG(LogTemp, Error, TEXT("InputKeySelector is INVALID."));
+		return;
+	}
+	InputKeySelector->OnKeySelected.AddDynamic(this, &ThisClass::OnKeySelected);
+	InputKeySelector->OnIsSelectingKeyChanged.AddDynamic(this, &ThisClass::OnIsSelectingKeyChanged);
+	bIsKeyChanged = false;
+
+	if (!DefaultButton)
+	{
+		UE_LOG(LogTemp, Error, TEXT("DefaultButton is INVALID."));
+		return;
+	}
+	DefaultButton->OnClicked.AddDynamic(this, &ThisClass::OnDefaultButtonClicked);
+
+	if (!ClearButton)
+	{
+		UE_LOG(LogTemp, Error, TEXT("ClearButton is INVALID."));
+		return;
+	}
+	ClearButton->OnClicked.AddDynamic(this, &ThisClass::OnClearButtonClicked);
+}
+
+void UGRKeySettingSlot::SetKeyMapping(const FPlayerKeyMapping& InMapping)
+{
+	Mapping = InMapping;
+
+	SetMappingName(Mapping.GetMappingName());
+	SetActionText(Mapping.GetDisplayName());
+	SetKey(Mapping.GetCurrentKey());
+
+	SetDefaultButtonVisibility(Mapping.GetCurrentKey());
+}
+
+void UGRKeySettingSlot::SetParentWidget(UGRKeySettingWidget* InParentWidget)
+{
+	ParentWidget = InParentWidget;
+}
+
+void UGRKeySettingSlot::SetMappingName(const FName& InName)
+{
+	MappingName = InName;
+}
+
+void UGRKeySettingSlot::SetActionText(const FText& InText)
+{
+	if (!ActionText)
+	{
+		UE_LOG(LogTemp, Error, TEXT("ActionText is INVALID."));
+		return;
+	}
+
+	ActionText->SetText(InText);
+}
+
+void UGRKeySettingSlot::SetKey(const FKey& InKey)
+{
+	if (!InputKeySelector)
+	{
+		UE_LOG(LogTemp, Error, TEXT("InputKeySelector is INVALID."));
+		return;
+	}
+
+	InputKeySelector->SetSelectedKey(FInputChord(InKey));
+}
+
+void UGRKeySettingSlot::SetDefaultButtonVisibility(const FKey& InCurrentKey)
+{
+	if (!DefaultButton)
+	{
+		UE_LOG(LogTemp, Error, TEXT("DefaultButton (UButton) is INVALID."));
+		return;
+	}
+
+	if (InCurrentKey == Mapping.GetDefaultKey())
+	{
+		DefaultButton->SetVisibility(ESlateVisibility::Hidden);
+	}
+	else
+	{
+		DefaultButton->SetVisibility(ESlateVisibility::Visible);
+	}
+}
+
+void UGRKeySettingSlot::NotifyToParent(const FKey& NewKey)
+{
+	if (ParentWidget.IsValid())
+	{
+		UGRKeySettingWidget* Parent = ParentWidget.Get();
+		Parent->UpdateMappings(MappingName, NewKey);
+	}
+}
+
+void UGRKeySettingSlot::OnKeySelected(FInputChord SelectedKey)
+{
+	if (!bIsKeyChanged)
+	{
+		return;
+	}
+
+	bIsKeyChanged = false;
+	NotifyToParent(SelectedKey.Key);
+	SetDefaultButtonVisibility(SelectedKey.Key);
+}
+
+void UGRKeySettingSlot::OnIsSelectingKeyChanged()
+{
+	bIsKeyChanged = true;
+}
+
+void UGRKeySettingSlot::OnDefaultButtonClicked()
+{
+	if (!InputKeySelector)
+	{
+		UE_LOG(LogTemp, Error, TEXT("InputKeySelector (UInputKeySelector) is INVALID."));
+		return;
+	}
+
+	const FKey& DefaultKey = Mapping.GetDefaultKey();
+	InputKeySelector->SetSelectedKey(FInputChord(DefaultKey));
+	NotifyToParent(DefaultKey);
+	SetDefaultButtonVisibility(DefaultKey);
+}
+
+void UGRKeySettingSlot::OnClearButtonClicked()
+{
+	if (!InputKeySelector)
+	{
+		UE_LOG(LogTemp, Error, TEXT("InputKeySelector (UInputKeySelector) is INVALID."));
+		return;
+	}
+
+	const FKey& NoneKey = FKey();
+	InputKeySelector->SetSelectedKey(FInputChord(NoneKey));
+	NotifyToParent(NoneKey);
+	SetDefaultButtonVisibility(NoneKey);
+}
