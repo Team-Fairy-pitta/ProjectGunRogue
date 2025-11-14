d540624f56b0d6f15ea4a1ab2a8ecbe23a4c1a63
Daegeun Choe <asdf12gg@gmail.com>
2025-11-14 20:28:20 +0900

    GREffect에 ICON 정보와 이름, 설명을 추가할 수 있도록 함

--- Source/GunRogue/AbilitySystem/GRGameplayEffect.h
+++ Source/GunRogue/AbilitySystem/GRGameplayEffect.h
@@ -3,9 +3,20 @@
 #include "GameplayEffect.h"
 #include "GRGameplayEffect.generated.h"
 
+class UTexture;
+
 UCLASS()
 class GUNROGUE_API UGRGameplayEffect : public UGameplayEffect
 {
 	GENERATED_BODY()
 	
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GunRogue|Icon")
+	TObjectPtr<UTexture> EffectIcon;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GunRogue|Name")
+	FText EffectName;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GunRogue|Description")
+	FText EffectDescription;
 };
