68ce761cc6ea5662e3f9945c4004fd99a0025892
Daegeun Choe <asdf12gg@gmail.com>
2025-11-17 12:00:47 +0900

    전투 게임을 위한 PC와 로비를 위한 PC 분리

---
+++ Source/GunRogue/Player/Battle/GRBattlePlayerController.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "Player/GRPlayerController.h"
+#include "GRBattlePlayerController.generated.h"
+
+UCLASS()
+class GUNROGUE_API AGRBattlePlayerController : public AGRPlayerController
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/GunRogue/Player/Lobby/GRLobbyPlayerController.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "Player/GRPlayerController.h"
+#include "GRLobbyPlayerController.generated.h"
+
+UCLASS()
+class GUNROGUE_API AGRLobbyPlayerController : public AGRPlayerController
+{
+	GENERATED_BODY()
+	
+};
