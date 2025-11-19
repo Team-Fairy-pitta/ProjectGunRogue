ed8a2cb8c199c02b8b234671cf799eecc65c393d
Daegeun Choe <asdf12gg@gmail.com>
2025-11-19 14:50:10 +0900

    테스트용 아이템 더 추가, 아이템 DataTable 추가

---
+++ Source/GunRogue/Item/GRItemInfoTableRow.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "Engine/DataTable.h"
+#include "GRItemInfoTableRow.generated.h"
+
+class UGRItemDefinition;
+
+UENUM(BlueprintType)
+enum class EItemRarity : uint8
+{
+	NORMAL UMETA(DisplayName = "Normal Item"),
+	RARE UMETA(DisplayName = "Rare Item"),
+	EPIC UMETA(DisplayName = "Epic Item")
+};
+
+USTRUCT(BlueprintType)
+struct FGRItemInfoTableRow : public FTableRowBase
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<UGRItemDefinition> ItemDefinition;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	EItemRarity Rarity;
+
+};
+
+
