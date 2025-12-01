d6591742b5c9e6b28ba9db412fc9e5e07b476737
Daegeun Choe <asdf12gg@gmail.com>
2025-12-01 10:59:55 +0900

    일인칭 조정, 비무장 상태에서 목이 움직이지 않던 문제 해결

--- Source/GunRogue/Character/GRCharacter_Camera.cpp
+++ Source/GunRogue/Character/GRCharacter_Camera.cpp
@@ -90,7 +90,7 @@ void AGRCharacter::ApplySmoothCameraControl_CameraArm(float DeltaTime)
 			SetCharacterVisibilityForCamera(true);
 		}
 		/* Magic Number: 1인칭 시점에서 캐릭터 표시 */
-		else if (SpringArmComponent->TargetArmLength <= 5)
+		else if (SpringArmComponent->TargetArmLength <= 1.0f)
 		{
 			SetCharacterVisibilityForCamera(true);
 		}
@@ -147,7 +147,7 @@ void AGRCharacter::AttachCameraArmToHead()
 	if (SpringArmComponent && GetMesh())
 	{
 		FName SocketName(TEXT("J_Bip_C_Head"));
-		SpringArmComponent->AttachToComponent(GetMesh(), FAttachmentTransformRules::KeepRelativeTransform, SocketName);
+		SpringArmComponent->AttachToComponent(GetMesh(), FAttachmentTransformRules::SnapToTargetNotIncludingScale, SocketName);
 		bIsCameraAttachedToHead = true;
 	}
 }
