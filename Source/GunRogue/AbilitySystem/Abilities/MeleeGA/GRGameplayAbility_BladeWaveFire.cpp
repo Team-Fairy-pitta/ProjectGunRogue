26ee4c1a2ab4a0ab11ef03864e2efa065d4e6647
lht2480 <jgjjgiggk@gmail.com>
2025-12-30 17:56:20 +0900

    요청 수정 사항
    1. 총을 들지 않아도 rush스킬 사용할 수 있도록 수정
    2. 검기가 검에서 날라가도록 수정(소켓 부착)
    3. 휘두르는 타이밍에 맞춰서 검기가 날아가도록 수정(애님 노티파이 사용)
    4. BladeWave 데미지 계산 로직, 프로젝타일로 옮김
    5. 스폰로직은 애님노티파이로 옮김

--- Source/GunRogue/AbilitySystem/Abilities/MeleeGA/GRGameplayAbility_BladeWaveFire.cpp
+++ Source/GunRogue/AbilitySystem/Abilities/MeleeGA/GRGameplayAbility_BladeWaveFire.cpp
@@ -2,13 +2,10 @@
 
 #include "AbilitySystem/GRAbilitySystemComponent.h"
 #include "AbilitySystem/Attributes/GRSkillAttributeSet_MeleeSkill.h"
-#include "AbilitySystem/Abilities/MeleeGA/GRBladeWaveProjectile.h"
 #include "Abilities/Tasks/AbilityTask_PlayMontageAndWait.h"
 
 #include "Engine/World.h"
 #include "GameFramework/Character.h"
-#include "Components/SkeletalMeshComponent.h"
-#include "Character/GRCharacter.h"
 
 UGRGameplayAbility_BladeWaveFire::UGRGameplayAbility_BladeWaveFire()
 {
@@ -16,8 +13,6 @@ UGRGameplayAbility_BladeWaveFire::UGRGameplayAbility_BladeWaveFire()
 	NetExecutionPolicy = EGameplayAbilityNetExecutionPolicy::ServerInitiated;
 
 	Tag_BladeWaveMode				= FGameplayTag::RequestGameplayTag(TEXT("State.BladeWaveMode"));
-	Tag_SizeAndDamageUp				= FGameplayTag::RequestGameplayTag(TEXT("Augment.BladeWave.SizeAndDamageUp"));
-	Tag_SlowPierceAndDamageUp		= FGameplayTag::RequestGameplayTag(TEXT("Augment.BladeWave.SlowPierceDamageUp"));
 	Tag_KillReduceSupportCooldown	= FGameplayTag::RequestGameplayTag(TEXT("Augment.BladeWave.KillReduceCooldown"));
 
 	ActivationRequiredTags.AddTag(Tag_BladeWaveMode);
@@ -61,7 +56,7 @@ void UGRGameplayAbility_BladeWaveFire::ActivateAbility(
 	const UGRSkillAttributeSet_MeleeSkill* SkillSet = GetSkillSet();
 	ACharacter* OwnerChar = Cast<ACharacter>(GetAvatarActorFromActorInfo());
 
-	if (!GRASC || !SkillSet || !OwnerChar || !ProjectileClass)
+	if (!GRASC || !SkillSet || !OwnerChar)
 	{
 		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
 		return;
@@ -69,10 +64,7 @@ void UGRGameplayAbility_BladeWaveFire::ActivateAbility(
 
 	float FireInterval = SkillSet->GetBladeWave_BaseFireInterval();
 
-	const bool bSlowPierce = GRASC->HasMatchingGameplayTag(Tag_SlowPierceAndDamageUp);
-	const bool bPierce = bSlowPierce;
-
-	if (bSlowPierce)
+	if (GRASC->HasMatchingGameplayTag(Tag_SlowPierceAndDamageUp))
 	{
 		FireInterval *= SkillSet->GetBladeWave_SlowFireIntervalMultiplier();
 	}
@@ -84,16 +76,6 @@ void UGRGameplayAbility_BladeWaveFire::ActivateAbility(
 		return;
 	}
 	LastFireTimeSeconds = Now;
-
-	float Damage = SkillSet->GetBladeWave_BaseDamage();
-	float WaveScale = SkillSet->GetBladeWave_BaseWaveScale();
-
-	const bool bSizeUp = GRASC->HasMatchingGameplayTag(Tag_SizeAndDamageUp);
-	if (bSizeUp)
-	{
-		WaveScale *= SkillSet->GetBladeWave_SizeAndDamageUp_ScaleMultiplier();
-		Damage *= SkillSet->GetBladeWave_DamageMultiplier();
-	}
 
 	if (FireMontage)
 	{
@@ -118,16 +100,6 @@ void UGRGameplayAbility_BladeWaveFire::ActivateAbility(
 		PlayMontageTask->OnInterrupted.AddDynamic(this, &UGRGameplayAbility_BladeWaveFire::OnFireMontageCancelled);
 		PlayMontageTask->OnCancelled.AddDynamic(this, &UGRGameplayAbility_BladeWaveFire::OnFireMontageCancelled);
 		PlayMontageTask->ReadyForActivation();
-	}
-
-	if (ActorInfo->IsNetAuthority())
-	{
-		const bool bSpawned = SpawnProjectileServer(Damage, WaveScale, bPierce);
-		if (!bSpawned)
-		{
-			EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
-			return;
-		}
 	}
 }
 
@@ -155,43 +127,4 @@ void UGRGameplayAbility_BladeWaveFire::OnFireMontageCompleted()
 void UGRGameplayAbility_BladeWaveFire::OnFireMontageCancelled()
 {
 	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, true);
-}
-
-bool UGRGameplayAbility_BladeWaveFire::SpawnProjectileServer(float Damage, float WaveScale, bool bPierce) const
-{
-	UWorld* World = GetWorld();
-
-	AGRCharacter* OwnerChar = Cast<AGRCharacter>(GetAvatarActorFromActorInfo());
-	if (!World || !OwnerChar || !ProjectileClass)
-	{
-		return false;
-	}
-
-	USkeletalMeshComponent* WeaponMesh = OwnerChar->GetEquippedWeaponMesh();
-
-	FVector SpawnLocation = OwnerChar->GetActorLocation();
-	if (WeaponMesh && WeaponMesh->DoesSocketExist(MuzzleSocketName))
-	{
-		SpawnLocation = WeaponMesh->GetSocketLocation(MuzzleSocketName);
-	}
-
-	const FRotator SpawnRotation = OwnerChar->GetBaseAimRotation();
-	const FTransform SpawnTM(SpawnRotation, SpawnLocation);
-
-	AGRBladeWaveProjectile* Proj = World->SpawnActorDeferred<AGRBladeWaveProjectile>(
-		ProjectileClass,
-		SpawnTM,
-		OwnerChar,
-		OwnerChar,
-		ESpawnActorCollisionHandlingMethod::AlwaysSpawn);
-
-	if (!Proj)
-	{
-		return false;
-	}
-
-	Proj->InitProjectile(Damage, WaveScale, bPierce);
-	Proj->FinishSpawning(SpawnTM);
-
-	return true;
 }
